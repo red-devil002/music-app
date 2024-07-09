be9c460fb87e692e6e3d700da2fff11f689cecb8
Swetang Pandit <swetangpandit@gmail.com>
2024-07-09 18:06:30 +0530

    Hero Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.2]">
-      <h1>HELOO HEloo</h1>
+      <HeroSection />
     </main>
   );
 }
